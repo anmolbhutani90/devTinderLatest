126b67717428ba5e53f9daede1ad1d9e0bab0070
Malvika <malvika@Malvikas-MacBook-Air.local>
2026-05-28 09:27:37 +0530

    removed db creds

--- config/database.js
+++ config/database.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://devtinderLatest:QKIeIWuTu0pP2f8h@cluster0.wyy19va.mongodb.net/devTinderLatest')
-}
+
 
 module.exports = connectDB;
