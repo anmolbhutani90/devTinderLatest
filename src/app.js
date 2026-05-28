f4ead8512fe2e1b23d755eb5b950e31f280ed54b
Malvika <malvika@Malvikas-MacBook-Air.local>
2026-05-28 11:07:40 +0530

    crud operations using mongoose done

--- src/app.js
+++ src/app.js
@@ -14,10 +14,53 @@ app.post("/signup", async(req,res)=>{
     }catch(err){
         res.status(400).send("Error in user adding" + err.message)
     }
-    
-
 })
 
+app.get('/feed', async(req,res)=>{
+    try{
+        const users = await User.find({})
+        console.log(users);
+        if(users.length === 0) {
+            res.status(404).send("User not found")
+        }else{
+            res.send("Users found")
+        }
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
+app.get('/user', async(req,res)=>{
+    try{
+        const userEmailId = req.body.userEmailId;
+        const user = await User.findOne({emailId:userEmailId}).exec();
+        if(!user){
+            res.status(400).send("User not found with "+ userEmailId);
+        }
+        res.send("User found")
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
+app.delete('/user', async (req,res)=>{
+    try{
+        const userId = req.body.userId;
+        console.log(userId)
+        await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully")
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
+app.patch("/user", async (req,res) =>{
+    try{
+        const userId = req.body.userId;
+        const data = req.body;
+        await User.findByIdAndUpdate(userId,data);
+        res.send("User data updated successfully")
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
 
 connectDB()
 .then(()=>{
